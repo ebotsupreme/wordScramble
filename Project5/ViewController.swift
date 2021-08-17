ad0cb27f3c11139bd58ba0dca7d3aa477b00b9da
Eddie Jung <ejung716@gmail.com>
2021-08-17 09:08:34 -0700

    Add grand central dispatch for loading txt file.

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -17,17 +17,24 @@ class ViewController: UITableViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Start Game", style: .plain, target: self, action: #selector(startGame))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
-        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let startWords = try? String(contentsOf: startWordsURL) {
-                allWords = startWords.components(separatedBy: "\n")
+        DispatchQueue.global().async { [weak self] in
+            if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+                if let startWords = try? String(contentsOf: startWordsURL) {
+                    self?.allWords = startWords.components(separatedBy: "\n")
+                }
             }
+            
+            if ((self?.allWords.isEmpty) == nil) {
+                self?.allWords = ["silkworm"]
+            }
+            DispatchQueue.main.async {
+                self?.startGame()
+            }
+            
+            
         }
         
-        if allWords.isEmpty {
-            allWords = ["silkworm"]
-        }
         
-        startGame()
     }
 
     @objc func startGame() {
